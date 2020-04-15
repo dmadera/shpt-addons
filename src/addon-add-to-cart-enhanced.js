e314ddb7178b31d25c89ad8d8bd0868f40eb21f9
Daniel Maděra <madera.dan@gmail.com>
2020-04-16 01:33:17 +0200

    Tested and passed + design cart buttons Works in Firefox, Chrome, Edge No JS in IE 11 but can add to cart

--- src/addon-add-to-cart-enhanced.js
+++ src/addon-add-to-cart-enhanced.js
@@ -3,70 +3,86 @@ import data from './datalayer'
 
 var PREVENT_BLUR = false
 
-function appendUpdateForm(productDiv, action = 'update') {
+function processUpdateForm(productDiv, action = 'update') {
     const form = $('<form method="post" class="pr-action">')
+    const formWrapper = productDiv.find('.form-container')
     let amount = productDiv.find('.cart-amount').val()
     form.attr('action', '/action/Cart/setCartItemAmount/')
 
-    if (action == 'add') {
+    if (action == 'add' && amount > 0) {
         form.attr('action', '/action/Cart/addCartItem/')
     } else if (amount == 0) {
+        action = 'delete'
         form.attr('action', '/action/Cart/deleteCartItem/')
+        productDiv.find('.cart-amount').val(1)
     }
     const code = productDiv.attr('data-cart-code')
     const priceId = productDiv.attr('data-cart-priceid')
     const itemId = productDiv.attr('data-cart-itemid')
+
     form.append($('<input type="hidden" name="itemId" value="">').val(itemId))
     form.append($('<input type="hidden" name="priceId">').val(priceId))
     form.append($('<input type="hidden" name="amount">').val(amount))
-    productDiv.find('.form-container').html(form)
-    return form
+    console.log('Injecting form: ', action)
+    formWrapper.html(form)
+    formWrapper.find('form').submit()
+    formWrapper.html('')
 }
 
 function addElements(container) {
-    if ($('.addon.add-to-cart-enhanced').length == 0) {
+    container.find(".product").each(function () {
+        const productDiv = $(this)
+        if (productDiv.find('.addon.add-to-cart-enhanced').length > 0) return
+
         let wrapper = $('<div class="addon add-to-cart-enhanced">')
         wrapper.append('<div class="form-container">')
         const buttonsWrapper = $('<div class="cart-buttons-wrapper">')
         const buttonIncDecWrapper = $('<div class="cart-inc-dec-buttons-wrapper">')
         buttonIncDecWrapper
-            .append($('<button class="cart-increase-button">'))
             .append($('<button class="cart-decrease-button">'))
-            .append($('<div class="clearfix">'))
+            .append($('<button class="cart-increase-button">'))
         buttonsWrapper
-            .append(buttonIncDecWrapper)
-            .append($('<button class="cart-add-button">Do košíku</button>'))
-            .append($('<button class="cart-update-button">Nastavit</button>'))
             .append($('<input type="text" class="cart-amount" autocomplete="off" data-decimals="0" data-min="0" data-max="9999">'))
+            .append($('<button class="cart-add-button">Koupit</button>'))
+            .append($('<button class="cart-update-button">Nastavit</button>'))
+            .append(buttonIncDecWrapper)
         wrapper.append(buttonsWrapper)
-        container.find('.ratings-wrapper').append(wrapper)
-        container.find('.product').append($('<span class="in-cart-info">').append($('<span>')).append('ks v košíku'))
+        productDiv.find('.p-bottom').append(wrapper)
+        productDiv.append($('<span class="in-cart-info">').append($('<span>')).append('ks v košíku'))
 
-        container.find(".product").each(function () {
-            const productDiv = $(this).closest('.product')
-            let quantity = productDiv.attr('data-cart-quantity')
-            productDiv.find('.cart-amount').val(quantity == 0 ? 1 : quantity)
-            if (quantity > 0) {
-                showIncDesButton(productDiv)
-            } else {
-                showAddButton(productDiv)
-            }
-            updateCartInfo(productDiv, quantity)
-        })
+        let quantity = productDiv.attr('data-cart-quantity')
+        productDiv.find('.cart-amount').val(quantity == 0 ? 1 : quantity)
+        if (quantity > 0) {
+            showIncDesButton(productDiv)
+        } else {
+            showAddButton(productDiv)
+        }
 
-        $('.cart-amount')
+        productDiv.find('.cart-amount')
             .on('focus', function (e) {
+                e.preventDefault()
                 const productDiv = $(this).closest('.product')
                 PREVENT_BLUR = false
-                showUpdateButton(productDiv)
+                const addButton = productDiv.find('.cart-add-button')
+                if (!addButton.is(':visible')) {
+                    productDiv.attr('data-prev-edit', $(this).val())
+                    showUpdateButton(productDiv)
+                } else {
+                    productDiv.attr('data-prev-edit', 0)
+                }
             })
             .on('blur', function (e) {
-                const productDiv = $(this).closest('.product')
                 if (PREVENT_BLUR) {
-                    e.preventDefault();
                     return
                 }
-                productDiv.find('.cart-update-button').trigger('mousedown')
+                const productDiv = $(this).closest('.product')
+                const dataAmount = productDiv.attr('data-prev-edit')
+                if (dataAmount > 0) {
+                    productDiv.find('.cart-amount').val(dataAmount)
+                    showIncDesButton(productDiv)
+                } else {
+                    showAddButton(productDiv)
+                }
             })
             .on('keypress', function (e) {
                 const productDiv = $(this).closest('.product')
@@ -75,16 +91,21 @@ function addElements(container) {
                 }
                 const addButton = productDiv.find('.cart-add-button')
                 const updateButton = productDiv.find('.cart-update-button')
-                if (addButton.is(':visible')) addbutton.trigger('click')
+                if (addButton.is(':visible')) addButton.trigger('click')
                 else updateButton.trigger('mousedown')
             })
-        $('.cart-add-button').click(function (e) {
+
+        productDiv.find('.cart-add-button').click(function (e) {
+            e.preventDefault()
             const productDiv = $(this).closest('.product')
-            const form = appendUpdateForm(productDiv, 'add')
-            form.submit()
+            const val = productDiv.find('.cart-amount').val()
+            processUpdateForm(productDiv, 'add')
+            updateCartInfo(productDiv, val)
             showIncDesButton(productDiv)
         })
-        $('.cart-update-button').on('mousedown', function (e) {
+
+        productDiv.find('.cart-update-button').on('mousedown', function (e) {
+            e.preventDefault()
             PREVENT_BLUR = true
             const productDiv = $(this).closest('.product')
             const val = productDiv.find('.cart-amount').val()
@@ -93,42 +114,55 @@ function addElements(container) {
                 e.preventDefault()
                 return
             }
-            const form = appendUpdateForm(productDiv)
-            form.submit()
+            processUpdateForm(productDiv)
+            updateCartInfo(productDiv, val)
             if (val == 0) {
                 showAddButton(productDiv)
             } else {
                 showIncDesButton(productDiv)
             }
         })
-        $('.cart-inc-dec-buttons-wrapper').on('mouseenter', function () {
+
+        productDiv.find('.cart-inc-dec-buttons-wrapper').on('mouseenter', function (e) {
+            e.preventDefault()
             const productDiv = $(this).closest('.product')
             $(this).attr('data-amount', productDiv.find('.cart-amount').val())
         })
-        $('.cart-inc-dec-buttons-wrapper').on('mouseleave', function () {
+
+        productDiv.find('.cart-inc-dec-buttons-wrapper').on('mouseleave', function (e) {
+            e.preventDefault()
             const productDiv = $(this).closest('.product')
             productDiv.find('.cart-update-button').trigger('mousedown')
         })
-        $('.cart-increase-button').click(function (e) {
+
+        productDiv.find('.cart-increase-button').click(function (e) {
+            e.preventDefault()
             const productDiv = $(this).closest('.product')
             const val = parseInt(productDiv.find('.cart-amount').val())
             const newVal = val + 1
             productDiv.find('.cart-amount').val(newVal)
             updateCartInfo(productDiv, newVal)
         })
-        $('.cart-decrease-button').click(function (e) {
+
+        productDiv.find('.cart-decrease-button').click(function (e) {
+            e.preventDefault()
             const productDiv = $(this).closest('.product')
             const val = parseInt(productDiv.find('.cart-amount').val())
             const newVal = val == 0 ? 0 : val - 1
             productDiv.find('.cart-amount').val(newVal)
+            if (newVal == 0) {
+                const updateButton = productDiv.find('.cart-update-button')
+                updateButton.trigger('mousedown')
+                return
+            }
             updateCartInfo(productDiv, newVal)
         })
-    }
+    })
 }
 
 function updateProductsData(container) {
     console.log('Updating products data')
-    var items = {}
+    var items = []
     $.ajax({
         async: false,
         type: "GET",
@@ -141,25 +175,29 @@ function updateProductsData(container) {
         const productDiv = $(this)
         const productCode = productDiv.find('.p-code span').html()
         const productPriceId = productDiv.find('.p-tools input[name="priceId"]').val()
-
-        productDiv.attr('data-cart-quantity', 0)
-        productDiv.attr('data-cart-code', productCode)
-        productDiv.attr('data-cart-priceid', productPriceId)
-        productDiv.attr('data-cart-index', -1)
-        productDiv.attr('data-cart-itemid', "")
+        let quantity = 0,
+            itemId = '',
+            cartIndex = -1
 
         for (const [index, product] of data.getCart().entries()) {
             if (product.code == productCode) {
-                const itemId = $(items.get(index)).find('input[name="itemId"]').val()
-                const quantity = $(items.get(index)).find('input[name="amount"]').val()
-                productDiv.attr('data-cart-quantity', quantity)
-                productDiv.attr('data-cart-code', product.code)
-                productDiv.attr('data-cart-index', index)
-                productDiv.attr('data-cart-itemid', itemId)
+                itemId = $(items.get(index)).find('input[name="itemId"]').val()
+                quantity = $(items.get(index)).find('input[name="amount"]').val()
+                cartIndex = index
                 break
             }
         }
-        updateCartButtons(productDiv)
+
+        productDiv.attr('data-cart-code', productCode)
+        productDiv.attr('data-cart-priceid', productPriceId)
+        productDiv.attr('data-cart-index', cartIndex)
+        productDiv.attr('data-cart-quantity', quantity)
+        productDiv.attr('data-cart-itemid', itemId)
+
+        productDiv.find('.cart-amount').val(quantity == 0 ? 1 : quantity)
+        if (quantity == 0) showAddButton(productDiv)
+        else showIncDesButton(productDiv)
+        updateCartInfo(productDiv, quantity)
     })
 }
 
@@ -182,28 +220,6 @@ function showUpdateButton(productDiv) {
     addonWrapper.find('.cart-update-button').show()
 }
 
-function updateCartButtons(productDiv, update = false) {
-    const addonWrapper = productDiv.find('.addon.add-to-cart-enhanced')
-    const dataQuantity = productDiv.attr('data-cart-quantity')
-    const inputAmount = productDiv.find('.cart-amount').val()
-
-    addonWrapper.find('button').hide()
-    if (dataQuantity == 0) {
-        addonWrapper.find('.cart-add-button').show()
-        productDiv.find('.cart-amount').val(1)
-    } else {
-        if (update) {
-            addonWrapper.find('button').hide()
-            addonWrapper.find('.cart-update-button').show()
-        } else {
-            addonWrapper.find('.cart-increase-button').show()
-            addonWrapper.find('.cart-decrease-button').show()
-        }
-    }
-    if (!update)
-        updateCartInfo(productDiv, dataQuantity)
-}
-
 function updateCartInfo(productDiv, amount) {
     if (amount == 0) {
         productDiv.find('.in-cart-info span').html('')
@@ -216,19 +232,20 @@ function updateCartInfo(productDiv, amount) {
 
 $(document).ready(function () {
     var CONTAINER = $('.products:not(.products-top)')
-    updateProductsData(CONTAINER)
     addElements(CONTAINER)
+    updateProductsData(CONTAINER)
 })
 
 $(document).on("ShoptetDOMContentLoaded", (event) => {
     var CONTAINER = $('.products:not(.products-top)')
-    updateProductsData(CONTAINER)
     addElements(CONTAINER)
+    updateProductsData(CONTAINER)
 })
 
 
 $(document).on("ShoptetCartUpdated", (event) => {
     var CONTAINER = $('.products:not(.products-top)')
+    addElements(CONTAINER)
     updateProductsData(CONTAINER)
-})
 
+})
